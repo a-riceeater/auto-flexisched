57f55aebc9e42f57d3a4a083ba7ffa069576aaa4
darthvader1925 <elijb12@gmail.com>
2023-04-22 22:41:58 -0700

    node-fetch

--- src/serv.js
+++ src/serv.js
@@ -38,7 +38,7 @@ app.post("/api/sched", (req, res) => {
     if (!regex.test(endpoint)) return res.send({ error: true, message: "Invalid endpoint!" })
 
 
-    console.log(`Fetching to endpoint ${endpoint}... \nDay: ${day} \nTeacher: ${fname} ${lname} \nInterval: ${interval} \nToken: ${token.slice(0, 5)}...`)
+    console.log(`Fetching to endpoint ${endpoint}... \nDay: ${day} \nTeacher: ${fname} ${lname} \nInterval: ${interval} \nToken: ${token.slice(0, 8)}...`)
     fetch("https://hpms.flexisched.net/clickToSched.php", {
         "headers": {
             "accept": "*/*",
